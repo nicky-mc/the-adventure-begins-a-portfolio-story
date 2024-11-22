63a092ef465ce855da0cdb138eb054702de4d7a8
nicky-mc <cinimod@gmail.com>
2024-11-22 20:38:35 +0000

    dropdown added for mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -27,7 +27,7 @@ function Header() {
           </label>
           <ul
             tabIndex={0}
-            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-black rounded-box w-52"
           >
             <li>
               <Link href="/">Home</Link>
